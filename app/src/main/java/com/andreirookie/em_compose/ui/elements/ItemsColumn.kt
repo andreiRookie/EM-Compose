ec3c34514769d76c14fd817e1d7bff45bd156bb6
AndreiRookie <andrey.idem@gmail.com>
2025-01-04 13:14:10 +0400

    illegalStateException due to nested LAzyColumn

---
+++ app/src/main/java/com/andreirookie/em_compose/ui/elements/ItemsColumn.kt
@@ -0,0 +1,32 @@
+package com.andreirookie.em_compose.ui.elements
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun ItemsColumn() {
+    LazyColumn() {
+        item {
+            Text(text = "First item")
+        }
+        items(5) { index ->
+            Text(text = "Item: $index")
+// IllegalStateException: Vertically scrollable component was measured with an infinity
+// maximum height constraints, which is disallowed.
+// One of the common reasons is nesting layouts like LazyColumn and Column(Modifier.verticalScroll())
+//            LazyColumn() {
+//                items(1) { indexInternal ->
+//                    Text(text = "Item: $indexInternal")
+//                }
+//            }
+        }
+
+        // Add another single item
+        item {
+            Text(text = "Last item")
+        }
+    }
+}
